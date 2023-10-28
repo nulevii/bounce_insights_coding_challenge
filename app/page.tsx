e09b90cb3e86c3ba42ffa66be1f46116ecf1cda0
Cherniugov Yevgenii <cherniugov@gmail.com>
2023-10-28 20:36:36 +0100

    Fix  building

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,23 @@ import { ICountriesList } from "./country-search/countrie-search.types";
 import SelectBlock from "./country-search/select-block";
 import { Providers } from "./country-search/provider";
 
-// async function getCountryNames() {
-//     const res = await fetch(COUNTRIES_LIST_URL, { next: { revalidate: 1 } });
-//     if (!res.ok) {
-//         throw new Error("Failed to fetch data");
-//     }
-//     const result = await res.json();
-//     return result.data;
-// }
+async function getCountryNames() {
+    const res = await fetch(COUNTRIES_LIST_URL, { next: { revalidate: 1 } });
+    if (!res.ok) {
+        throw new Error("Failed to fetch data");
+    }
+    const result = await res.json();
+    return result.data;
+}
 
 export default async function CountrySearch() {
-    // const countryNamesResponse: ICountriesList = await getCountryNames();
-    // const countryNamesList = Object.entries(countryNamesResponse).map(
-    //     ([key, value]) => ({ value: key, label: value.country })
-    // );
+    const countryNamesResponse: ICountriesList = await getCountryNames();
+    const countryNamesList = Object.entries(countryNamesResponse).map(
+        ([key, value]) => ({ value: key, label: value.country })
+    );
     return (
-        // <Providers>
-        //     <SelectBlock countryNamesList={countryNamesList} />
-        // </Providers>
-        <div>hello</div>
+        <Providers>
+            <SelectBlock countryNamesList={countryNamesList} />
+        </Providers>
     );
 }
