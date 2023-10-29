a48d25af4bfc2259f6e2c3044f5693f9fa632b7a
Cherniugov Yevgenii <cherniugov@gmail.com>
2023-10-29 12:44:04 +0000

    urls fix

--- backend/src/urls.ts
+++ backend/src/urls.ts
@@ -1,3 +1,2 @@
-export const COUNTRIES_LIST_URL =
-    "https://api.first.org/data/v1/countries?limit=254";
+export const COUNTRIES_LIST_URL = "https://api.first.org/data/v1/countries?limit=254";
 export const COUTRY_URL = "https://restcountries.com/v3.1/alpha/";
